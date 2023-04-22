2d2c48134854016322f05140529e1b9d3d87b6c1
amineglr <aminedglrglr@gmail.com>
2023-04-22 18:02:00 +0300

    create profile model and add it to the admin panel

--- storyTellerApp/models.py
+++ storyTellerApp/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
 # Create your models here.
+
+User = get_user_model()
+
+
+class Profile(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user_id = models.IntegerField()
+    first_name = models.CharField(max_length=100, default="")
+    last_name = models.CharField(max_length=100,  default="")
+    username = models.CharField(max_length=50, unique=True, default="")
+    email = models.EmailField(unique=True, default="")
+    password = models.CharField(max_length=128, default="")
+    profile_picture = models.ImageField(
+        upload_to='profile_picture', default='')
+    bio = models.TextField(blank=True)
+
+    
