233d82037491f3cdf1475777bcddf6647783e3ae
amineglr <aminedglrglr@gmail.com>
2023-04-28 18:11:04 +0300

    admin page display configuration

--- storyTellerApp/admin.py
+++ storyTellerApp/admin.py
@@ -1,9 +1,14 @@
 from django.contrib.gis import admin
 from .models import Profile, Tag, Story , Comment ,Like , Location , Follower
 
+
+class StoryAdmin(admin.ModelAdmin):
+    list_filter =("user", "tags")
+    list_display =("title","user")
+
 admin.site.register(Profile)
 admin.site.register(Tag)
-admin.site.register(Story)
+admin.site.register(Story, StoryAdmin)
 admin.site.register(Like)
 admin.site.register(Location)
 admin.site.register(Follower)
