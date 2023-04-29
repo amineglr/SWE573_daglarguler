2bd720243bae047ceb32943ac6f7bb898425862d
amineglr <aminedglrglr@gmail.com>
2023-04-29 14:59:38 +0300

    login,logout and signup view and functions

--- storyTellerApp/urls.py
+++ storyTellerApp/urls.py
@@ -2,11 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # path("", views.index),
-    # path("<str:page>", views.pages, name="pages"),
     path("", views.home_page, name="home_page"),
     path("stories", views.stories, name="stories_page"),
     path("stories/<slug:id>", views.view_story, name="view_story"),
     path("signup", views.signup, name="signup"),
     path("login", views.login, name="login"),
+    path("signout", views.login, name="signout"),
+
 ]
--- storyTellerApp/views.py
+++ storyTellerApp/views.py
@@ -1,41 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse, HttpResponseNotFound
 from django.urls import reverse
 from django.template.loader import render_to_string
 from datetime import date
 from .models import Story
+from .models import Profile
+from django.contrib.auth.models import User
+from django.contrib import auth
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
-pages_list = {
-    "home": "Home!",
-    "stories": "Stories",
-    "mystories": "My Stories",
-    "likedstories": "Liked Stories",
-    "search": "Search",
-    "addstories": "Add Stories",
-}
-
-def index(request):
-    list_items = ""
-    pages = list(pages_list.keys())
-
-    for page in pages:
-        page_path = reverse("pages", args=[page])
-        list_items += f"<li><a href=\"{page_path}\">{page}</a></li>"
-
-    response_data = f"<ul>{list_items}</ul>"
-    return HttpResponse(response_data)
-
-
-def pages(request, page):
-    try:
-        page_text = pages_list[page]
-        response_data = render_to_string("storyTellerApp/home.html")
-        return HttpResponse(response_data)
-    except:
-        HttpResponseNotFound("Page not found!")
-
-
+@login_required(login_url='login')
 def home_page(request):
     latest_stories = Story.objects.all().order_by("-created_at")[:10]
     return render(request, "storyTellerApp/home.html",{ "stories" : latest_stories} )
@@ -60,8 +36,53 @@ def location(request):
     return HttpResponse("Location")
 
 def signup(request):
-    return render(request, 'storyTellerApp/signup.html')
+
+    if request.method == "POST":
+        username= request.POST['username']
+        email= request.POST['email']
+        password= request.POST['password']
+        password2= request.POST['password2']
+
+        if password== password2:
+            if User.objects.filter(email=email).exists():
+                messages.info(request,"Email taken")
+                return redirect('signup')
+            elif User.objects.filter(username=username).exists():
+                messages.info(request,"Username taken")
+                return redirect('signup')
+            else: 
+                user = User.objects.create_user(username=username, email=email,password=password)
+                user.save()
+
+                user_model=User.objects.get(username=username)
+                new_profile= Profile.objects.create(user=user_model, id_user=user_model.id)
+                new_profile.save()
+                return redirect('login')
+        else:
+            messages.info(request, "Password not maching")
+            return redirect('signup')
+
+    else:
+        return render(request, 'storyTellerApp/signup.html')
 
 
 def login(request):
-    return render(request, 'storyTellerApp/login.html')
+    if request.method =="POST":
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user= auth.authenticate(username=username, password=password)
+
+        if user is not None:
+            auth.login(request,user)
+            return redirect('home_page')
+        else:
+            messages.info(request, 'Invalid creditials')
+            return redirect('login')
+    else:
+        return render(request, 'storyTellerApp/login.html')
+
+@login_required(login_url='login')    
+def logout(request):
+    auth.logout(request)
+    return redirect('login')
